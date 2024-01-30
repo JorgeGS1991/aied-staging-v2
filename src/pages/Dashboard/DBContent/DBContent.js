e3e3fda4b4695abad99a4d90fcf1c30d7895d015
phucnguyen290198@gmail.com <anicedream2002@gmail.com>
2024-01-30 14:46:17 -0600

    edit dashboard

--- src/pages/Dashboard/DBContent/DBContent.js
+++ src/pages/Dashboard/DBContent/DBContent.js
@@ -15,30 +15,10 @@ function DBContent() {
         Congratulations on starting a rewarding learning journey!
       </Typography>
 
-      <Typography variant="h5" sx={{ my: 2 }} gutterBottom>
-        What is Work-Learn?
-      </Typography>
       <Typography>
-        The Work-Learn project is intended to prepare you for careers in the
-        Tech sector. In this curriculum, you will learn about three main topics:
-        Computational Thinking, COBOL, and Python. They are designed by ______
-        And more details/broad info about how the curriculum is structured
-        (Maggie or Phillip might know how to write this better than I do)…
-      </Typography>
-
-      <hr style={{ margin: "20px 0" }} />
-
-      <Typography variant="h5" sx={{ my: 2 }}>
-        Project and the Team
-      </Typography>
-      <Typography>
-        This curriculum is currently being piloted through National Science
-        Foundation (Grant #___). This is a joint venture between the University
-        of Nebraska Omaha, Southern Methodist University, and Sienna Francis
-        House. The project team consists of Dr. Magie Hall (Principal
-        Investigator), Dr. Michelle Friend (Co-PI), Dr. Alexandra Pavlakis
-        (Co-PI), and Dr, Wang Min (Postdoctoral Fellow). If you have any
-        questions about the project or the curriculum, please contact ______
+        We are so excited to have you here! To get started, click on “How to
+        Navigate the Work-Learn Website” in the sidebar on the left side of the
+        screen.
       </Typography>
     </Box>
   );
