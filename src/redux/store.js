16a9b4e38dbbf34368268b53839080096e44f756
phucnguyen290198@gmail.com <anicedream2002@gmail.com>
2024-02-13 18:12:27 -0600

    dev env vars

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,7 +8,7 @@ import thunk from "redux-thunk";
 import { userReducer } from "../redux/reducers/userReducer";
 import quizReducer from "./reducers/quizReducer";
 
-console.log("testing dev");
+console.log("testing develop");
 const rootReducer = combineReducers({
   user: userReducer,
   quiz: quizReducer,
