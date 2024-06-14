efd5e504c940dc59160fc3be441c609883ae8c5a
Temba7 <88758537+Temba7@users.noreply.github.com>
2024-06-14 15:21:47 -0500

    Update modules.js

--- src/data/modules.js
+++ src/data/modules.js
@@ -5872,37 +5872,8 @@ def area2(xc, yc, xp, yp):
      `
 	  
           },
-		{
-	     id: 4,
-            topic: "Quiz.....",
-            content: `
-            <div class='script-compiled'>
-               Coming very Soon...
-            </div>
-            <style>
-            .script-compiled p {
-              margin: 10px 0;
-            }
-	    .replit img {
-     		width: 50%
-       		height: auto;
-	 	text-align: center;
-   		margins: 20px;
-     		box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-       		border-radius: 5px; 
-	 }
-  	 .replit .lg-img {
-    		window: 70%; 
-      	}
-
-       .replit .xl-img {
-       		windth: 100%; 
-	 }
-  	.replit .sub-topic {
-   		margin-top: 30px;
-}
-     </style>
-     `
+		
+     
 
 		  
           },
