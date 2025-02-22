0b61a7845f178a9a8ae2c3ca5585db8a12ff3623
vadapallij <vadapallij@gmail.com>
2025-02-21 20:53:22 -0600

    1.modified login.js

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -17,8 +17,8 @@ function Login({ setUser, setUserRole }) {
   const handleLogin = async () => {
     try {
       const response = await axios.post(
-         "http://localhost:3001/login",
-        //`${process.env.REACT_APP_BACKEND_URL}/login`,
+         //"http://localhost:3001/login",
+        `${process.env.REACT_APP_BACKEND_URL}/login`,
         {
           username,
           password,
